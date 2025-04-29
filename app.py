fd5027be0fa7c28bae1d33e5bffd4f04cd978b45
OlVlas <mail@mail.org>
2025-04-29 14:03:46 +0300

    2222

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, Railway!"
+    return "Hello, Railway2!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
